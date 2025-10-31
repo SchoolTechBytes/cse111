279a327ea9e9e5bdf801a1ca73e165badb19aa74
SchoolTechBytes <nnixon@byupathway.edu>
2025-10-31 15:43:13 -0500

    Week 01 final project

--- week01/tire_volume.py
+++ week01/tire_volume.py
@@ -4,33 +4,14 @@
 # Instructor: Thomas Reid
 # Date: October 2025
 
-
-    # Have the user enter a tire width in mm.
-    # Have the user enter the aspect ratio.
-    # Have the user enter the diameter of the wheel in inches.
-    # Calculate and display the tire's volume.
-    # Log the information in a text file.
-    #     current date (Do NOT include time)
-    #     width of the tire
-    #     aspect ratio of the tire
-    #     diameter of the wheel
-    #     volume of the tire (rounded to two decimal places)
-
-
-# Milestone
-
-# Write a Python program named tire_volume.py that reads from the keyboard the three numbers for a tire and computes and outputs the volume of space inside that tire.
-#     Create a folder for this week's project, name it whatever you want.
-#     Open the folder you just created in VSCode.
-#     Create a file named tire_volume.py.
-#     Write the program to ask the user for the 3 items in the requirements and output the tire volume.
-
 import math
+from datetime import datetime
 
 tire_width = 0.0
 tire_ratio = 0.0
 tire_diameter = 0.0
 tire_volume = 0.0
+current_date = datetime.now()
 
 
 tire_width = float(input("Enter the width of the tire in mm (ex 205): "))
@@ -38,5 +19,9 @@ tire_ratio = float(input("Enter the aspect ratio of the tire (ex 60): "))
 tire_diameter = float(input("Enter the diameter of the wheel in inches (ex 15): "))
 
 tire_volume = (tire_width * tire_ratio + 2540 * tire_diameter) * (math.pi * tire_width ** 2 * tire_ratio) / 10000000000
+
+# write the following to tire_file date, width, ratio, diameter, volume
+with open("volumes.txt", "at") as tire_file:
+    print(f"{current_date:%Y-%m-%d}, {tire_width}, {tire_ratio}, {tire_diameter}, {tire_volume:.2f}", file=tire_file)
 
 print(f"The approximate volume is {tire_volume:.2f} liters")
