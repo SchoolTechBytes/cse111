7eecbabc720b3e394ead85e13f6f38bc613c9314
SchoolTechBytes <nnixon@byupathway.edu>
2025-11-11 22:51:29 -0600

    week 03

---
+++ week03/test_water_flow.py
@@ -0,0 +1,14 @@
+# Title: W03 Project: Water Pressure
+# Author: Nicholas Nixon
+# Class: CSE 111
+# Instructor: Thomas Reid
+# Date: November 2025
+
+
+from pytest import approx
+import pytest
+
+
+# Call the main function that is part of pytest so that the
+# computer will execute the test functions in this file.
+pytest.main(["-v", "--tb=line", "-rN", __file__])
