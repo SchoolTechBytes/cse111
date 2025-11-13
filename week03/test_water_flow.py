6be5f88d6178ec55722173b4f892913779eb1357
SchoolTechBytes <nnixon@byupathway.edu>
2025-11-12 21:59:51 -0600

    Week 03 update

--- week03/test_water_flow.py
+++ week03/test_water_flow.py
@@ -1,12 +1,38 @@
-# Title: W03 Project: Water Pressure
+# Title: W03 Project: Water Pressure Test Program
 # Author: Nicholas Nixon
 # Class: CSE 111
 # Instructor: Thomas Reid
 # Date: November 2025
 
-
+from water_flow import water_column_height, pressure_gain_from_water_height, pressure_loss_from_pipe
 from pytest import approx
 import pytest
+
+def test_water_column_height():
+    """Used to test the water_column_hight function
+    """
+    assert water_column_height(0.0, 0.0) == 0.0
+    assert water_column_height(0.0, 10.0) == 7.5
+    assert water_column_height(25.0, 0.0) == 25.0
+    assert water_column_height(48.3, 12.8) == 57.9
+
+def test_pressure_gain_from_water_height():
+    """Used to test the pressure_gain_from_water_height function
+    """
+    assert pressure_gain_from_water_height(0.0) == approx(0.000, rel=0.001)
+    assert pressure_gain_from_water_height(30.2) == approx(295.628, rel=0.001)
+    assert pressure_gain_from_water_height(50.0) == approx(489.450, rel=0.001)
+
+def test_pressure_loss_from_pipe():
+    """Used to test the pressure_loss_from_pipe function
+    """
+    assert pressure_loss_from_pipe(0.048692, 0.00, 0.018, 1.75) == approx(0.000, rel=0.001)
+    assert pressure_loss_from_pipe(0.048692, 200.00, 0.000, 1.75) == approx(0.000, rel=0.001)
+    assert pressure_loss_from_pipe(0.048692, 200.00, 0.018, 0.00) == approx(0.000, rel=0.001)
+    assert pressure_loss_from_pipe(0.048692, 200.00, 0.018, 1.75) == approx(-113.008, rel=0.001)
+    assert pressure_loss_from_pipe(0.048692, 200.00, 0.018, 1.65) == approx(-100.462, rel=0.001)
+    assert pressure_loss_from_pipe(0.286870, 1000.00, 0.013, 1.65) == approx(-61.576, rel=0.001)
+    assert pressure_loss_from_pipe(0.286870, 1800.75, 0.013, 1.65) == approx(-110.884, rel=0.001)
 
 
 # Call the main function that is part of pytest so that the
---
+++ week03/water_flow.py.old
@@ -0,0 +1,31 @@
+# Title: W03 Project: Water Pressure
+# Author: Nicholas Nixon
+# Class: CSE 111
+# Instructor: Thomas Reid
+# Date: November 2025
+
+
+
+def main():
+    pass
+
+def water_column_height(tower_height, tank_height):
+    pass
+
+def pressure_gain_from_water_height(height):
+    pass
+
+def pressure_loss_from_pipe(pipe_diameter, pipe_length, friction_factor, fluid_velocity):
+    pass
+
+def pressure_loss_from_fittings(fluid_velocity, quantity_fittings):
+    pass
+
+def reynolds_number(hydraulic_diameter, fluid_velocity):
+    pass
+
+def pressure_loss_from_pipe_reduction(larger_diameter, fluid_velocity, reynolds_number, smaller_diameter):
+    pass
+
+if __name__ == "__main__":
+    main()
