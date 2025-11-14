35f3d04fb2564bee32620ee583e287a5eed00475
SchoolTechBytes <nnixon@byupathway.edu>
2025-11-14 11:59:07 -0600

    Week 03 final update

--- week03/test_water_flow.py
+++ week03/test_water_flow.py
@@ -4,7 +4,7 @@
 # Instructor: Thomas Reid
 # Date: November 2025
 
-from water_flow import water_column_height, pressure_gain_from_water_height, pressure_loss_from_pipe
+from water_flow import water_column_height, pressure_gain_from_water_height, pressure_loss_from_pipe, pressure_loss_from_fittings, reynolds_number, pressure_loss_from_pipe_reduction
 from pytest import approx
 import pytest
 
@@ -34,6 +34,30 @@ def test_pressure_loss_from_pipe():
     assert pressure_loss_from_pipe(0.286870, 1000.00, 0.013, 1.65) == approx(-61.576, rel=0.001)
     assert pressure_loss_from_pipe(0.286870, 1800.75, 0.013, 1.65) == approx(-110.884, rel=0.001)
 
+def test_pressure_loss_from_fittings():
+    """Used to test pressure_loss_from_fittings function
+    """
+    assert pressure_loss_from_fittings(0.00, 3) == approx(0.000, abs=0.001)
+    assert pressure_loss_from_fittings(1.65, 0) == approx(0.000, abs=0.001)
+    assert pressure_loss_from_fittings(1.65, 2) == approx(-0.109, abs=0.001)
+    assert pressure_loss_from_fittings(1.75, 2) == approx(-0.122, abs=0.001)
+    assert pressure_loss_from_fittings(1.75, 5) == approx(-0.306, abs=0.001)
+
+def test_reynolds_number():
+    """Used to test reynolds_number function
+    """
+    assert reynolds_number(0.048692, 0.00) == approx(0, rel=1)
+    assert reynolds_number(0.048692, 1.65) == approx(80069, rel=1)
+    assert reynolds_number(0.048692, 1.75) == approx(84922, rel=1)
+    assert reynolds_number(0.286870, 1.65) == approx(471729, rel=1)
+    assert reynolds_number(0.286870, 1.75) == approx(500318, rel=1)
+
+def test_pressure_loss_from_pipe_reduction():
+    """Used to test pressure_loss_from_pipe_reduction function
+    """
+    assert pressure_loss_from_pipe_reduction(0.28687, 0.00, 1, 0.048692) == approx(0.000, abs=0.001)
+    assert pressure_loss_from_pipe_reduction(0.28687, 1.65, 471729, 0.048692) == approx(-163.744, abs=0.001)
+    assert pressure_loss_from_pipe_reduction(0.28687, 1.75, 500318, 0.048692) == approx(-184.182, abs=0.001)
 
 # Call the main function that is part of pytest so that the
 # computer will execute the test functions in this file.
--- week03/water_flow.py
+++ week03/water_flow.py
@@ -6,6 +6,10 @@ HDPE_SDR11_FRICTION_FACTOR = 0.018   # (unitless)
 HOUSEHOLD_VELOCITY = 1.75            # (meters / second)
 WATER_DENSITY=998.2                  # density of water (998.2 kilogram / meter^3)
 
+# Added the following variables for Exceeding the Requirements
+VISCOSITY_WATER=0.0010016            # dynamic viscosity of water (0.0010016 Pascal seconds)
+ACCELERATION_EARTH_GRAVITY=9.80665   # the acceleration from Earths gravity 9.80665 (meter / second2)
+
 def main():
     tower_height = float(input("Height of water tower (meters): "))
     tank_height = float(input("Height of water tank walls (meters): "))
@@ -37,7 +41,7 @@ def water_column_height(tower_height, tank_height):
     return tower_height + 3 * tank_height / 4
 
 def pressure_gain_from_water_height(height):
-    return (998.2 * 9.80665 * height) / 1000
+    return (WATER_DENSITY * ACCELERATION_EARTH_GRAVITY * height) / 1000
 
 def pressure_loss_from_pipe(pipe_diameter, pipe_length, friction_factor, fluid_velocity):
     numerator = -friction_factor * pipe_length * WATER_DENSITY * fluid_velocity ** 2
@@ -45,14 +49,13 @@ def pressure_loss_from_pipe(pipe_diameter, pipe_length, friction_factor, fluid_v
     return numerator / denominator
 
 def pressure_loss_from_fittings(fluid_velocity, quantity_fittings):
-    return -.04 * WATER_DENSITY * fluid_velocity * 2 * quantity_fittings / 2000
+    return -0.04 * WATER_DENSITY * fluid_velocity ** 2 * quantity_fittings / 2000
 
 def reynolds_number(hydraulic_diameter, fluid_velocity):
-      #TODO: Need to implement
-      return 0
+    return (WATER_DENSITY * hydraulic_diameter * fluid_velocity) / VISCOSITY_WATER
 
 def pressure_loss_from_pipe_reduction(larger_diameter, fluid_velocity, reynolds_number, smaller_diameter):
-    k=(.1 + 50 / reynolds_number) * ((larger_diameter / smaller_diameter) ** 4 + 1)
+    k=(.1 + 50 / reynolds_number) * ((larger_diameter / smaller_diameter) ** 4 - 1)
     return -k * WATER_DENSITY * fluid_velocity ** 2 / 2000
 
 if __name__ == "__main__":
